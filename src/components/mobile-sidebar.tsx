0a847734a54bd2cdd073722ac3304f02ce72ddf5
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-09-01 11:57:52 +0500

    feat - complete dashboard page

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -13,7 +13,18 @@ import {
 
 import { Button } from "./ui/button";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
