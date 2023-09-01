428ee6128ab5048cc23ccfa37deaabb15faa7298
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-09-02 00:45:31 +0500

    feat - complete landing page ui design

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { LandingContent } from "@/components/landing/landing-content";
+import { LandingHero } from "@/components/landing/landing-hero";
+import { LandingNavbar } from "@/components/landing/landing-navbar";
 
 const LandingPage = () => {
-  return <div>I\Unprotected LandingPage</div>;
+  return (
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+  );
 };
 
 export default LandingPage;
