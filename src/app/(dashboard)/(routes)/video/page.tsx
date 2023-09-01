164027716be2cc340e833fea3725ecca3864ffc8
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-09-01 17:46:14 +0500

    feat - add video and music generation AI modules

---
+++ src/app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { Music, Video } from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
+
+import { Heading } from "@/components/heading";
+
+import { formSchema } from "./constants";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+
+export default function VideoPage() {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+
+      const response = await axios.post("/api/video", values);
+      console.log(response);
+
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error) {
+      //* Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompt into videos"
+        icon={Video}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="A Parrot flying in amazon forest in the morning as the sun rises"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        {isLoading && (
+          <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+            <Loader />
+          </div>
+        )}
+
+        {!video && !isLoading && (
+          <div>
+            <Empty label="No video Started" />
+          </div>
+        )}
+        {video && (
+          <video
+            controls
+            className="w-full mt-8 aspect-video rounded-lg border bg-black"
+          >
+            <source src={video} />
+          </video>
+        )}
+      </div>
+    </div>
+  );
+}
